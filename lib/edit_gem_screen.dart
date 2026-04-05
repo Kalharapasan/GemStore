aa0168aabeb455f9e6f123fca829f61b9a6a4bcc
Pasan Kalhara <kalharap84@gmail.com>
2026-04-05 15:14:32 +0530

    Fix import paths for snackbar_utils in EditGemScreen and GemDetailScreen

--- lib/edit_gem_screen.dart
+++ lib/edit_gem_screen.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:gemstore2/utils/snackbar_utils.dart';
+import 'package:gemstore/utils/snackbar_utils.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'main.dart';
--- lib/gem_detail_screen.dart
+++ lib/gem_detail_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:gemstore2/utils/snackbar_utils.dart';
-import 'package:gemstore2/widgets/gem_dialog.dart';
+import 'package:gemstore/utils/snackbar_utils.dart';
+import 'package:gemstore/widgets/gem_dialog.dart';
 import 'main.dart';
 
 class GemDetailScreen extends StatefulWidget {
